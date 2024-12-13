bc81fd5bd6f0495988179e3502d681dd7fe8a62e
rishavtiwari22 <tiwaririshav463@gmail.com>
2024-12-13 17:48:59 +0530

    WE are trying to push our frist project of navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+let body = document.body;
+
+
+// window.addEventListener("resize", (e) => {
+//     let bodywidth = body.clientWidth;
+//     console.log('Body Width - ',bodywidth);
+//     if (bodywidth < 600) {
+//         let right_div = document.getElementsByClassName("right")[0];
+//         right_div.innerHTML = "<h1>Resize the window to see the magic</h1>";
+//     }
+// });
